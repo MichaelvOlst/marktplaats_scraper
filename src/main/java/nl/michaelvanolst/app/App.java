ba3d4987a4eaaadf4d18989e2df09d47529f7ae7
Michael van Olst <michaeltje@live.nl>
2022-03-05 10:17:06 +0100

    right trim the slash for the tasks dir

--- src/main/java/nl/michaelvanolst/app/App.java
+++ src/main/java/nl/michaelvanolst/app/App.java
@@ -1,16 +1,22 @@
 package nl.michaelvanolst.app;
 
 import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+
 import nl.michaelvanolst.app.Tasks.TaskCollector;
 
 public class App 
 {
-    public static void main( String[] args ) {
-        String taskDirectory = System.getProperty("user.dir") + "/tasks";
+    public static void main( String[] args ) throws IOException {
+        // String basePath = CharMatcher.whitespace().trimTrailingFrom(System.getProperty("user.dir"), "/");
+        String basePath = StringUtils.stripEnd(System.getProperty("user.dir"), "/");
+        String taskDirectory = basePath + "/tasks";
+
         TaskCollector taskCollector = new TaskCollector(taskDirectory);
         
         // try {
-        //     taskCollector.get();
+        //     System.out.println(taskCollector.get());
         // } catch(IOException ex) {
         //     System.out.println(ex.getStackTrace());
         // }
